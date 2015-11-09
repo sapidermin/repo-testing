f53a468bc17b1b0eae314a245eae4d917610c026
Nurul Huda <nurul.huda@inmagine.com>
2015-11-09 11:15:11 +0700

    add development

--- index.php
+++ index.php
@@ -2,9 +2,3 @@
 <header>huda was here again</header>
 <footer>footer feature</footer>
 <section class="feature">add section feature</section>
-<section>hotfix 1</section>
-<section>hotfix 2</section>
-<section>hotfix 3</section>
-<section>master 1</section>
-<section>master 2</section>
-<section>hotfix 4</section>
